d51318a590a1722fd8838d04880f921960c7b832
george-593 <63100333+george-593@users.noreply.github.com>
2025-06-29 22:25:27 +0100

    DB & Docker Setup

---
+++ db/init.sql
@@ -0,0 +1,16 @@
+CREATE TABLE public.airports (
+    id SERIAL PRIMARY KEY,
+    airportName varchar(255),
+    city varchar(255),
+    country varchar(255),
+    IATA varchar(3),
+    ICAO varchar(4),
+    latitude varchar(255),
+    longitude varchar(255),
+    altitude varchar(255),
+    timezone varchar(255),
+    DST varchar(255),
+    tzDBTimezone varchar(255),
+    airportType varchar(255),
+    source varchar(255),
+)
